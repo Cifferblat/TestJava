49bccab89e6ed99e938102101f040e4548621a84
Cifferblat <cifferblat777@gmail.com>
2020-07-06 12:07:28 +0300

    First commit

--- src/telran/persons/RandomPersonCreationAppl.java
+++ src/telran/persons/RandomPersonCreationAppl.java
@@ -38,10 +38,7 @@ public class RandomPersonCreationAppl {
 		
 		ConfigurableApplicationContext ctx = SpringApplication.run(RandomPersonCreationAppl.class, args);
 		
-		PersonsRepository repo = ctx.getBean(PersonsRepository.class);
-		
-//		String str = "Hello!";
-		
+		PersonsRepository repo = ctx.getBean(PersonsRepository.class);		
 		repo.saveAll(persons);
 	}
 
